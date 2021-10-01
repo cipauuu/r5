30531209f7c79226c0c9dc8deef90b8991a9bef0
BlueBeret <sofiruld@gmail.com>
2021-10-01 16:37:56 +0700

    add button tapi eventnya masi gabisa

--- src/components/Rekomendasi.vue
+++ src/components/Rekomendasi.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="rekomendasi" >
     <b-container fluid>
       <b-row>
         <b-col v-for="value in rekomendasi.slice(0,4)" :key="value" cols="3" class="d-flex align-items-stretch">
@@ -20,8 +20,29 @@
         </b-col>
       </b-row>
     </b-container>
+    <div id="refresh">
+      <button type="button" class="btn btn-dark" v-on:click="refresh"><i class="fa fa-refresh" aria-hidden="true"></i></button>
+    </div>
   </div>
 </template>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.18/vue.min.js"></script>
+
+<script>
+  const vm = new Vue({
+    el: '#rekomendasi',
+    data: {
+      name: 'Vue.js'
+    },
+    methods: {
+      refresh : function (event) {
+        alert('button is clicked')
+      }
+    }
+  })
+</script>
+
 
 <script>
 import { BContainer, BCard } from "bootstrap-vue";
@@ -52,5 +73,20 @@ export default {
 <style scoped>
 .card-img-top {
   height: 20vw;
+}
+
+#refresh {
+  /* display: none; Hidden by default */
+  position: fixed; /* Fixed/sticky position */
+  bottom: 20px; /* Place the button at the bottom of the page */
+  right: 30px; /* Place the button 30px from the right */
+  z-index: 99; /* Make sure it does not overlap */
+  border: none; /* Remove borders */
+  outline: none; /* Remove outline */
+  color: white; /* Text color */
+  cursor: pointer; /* Add a mouse pointer on hover */
+  padding: 15px; /* Some padding */
+  border-radius: 10px; /* Rounded corners */
+  font-size: 18px; /* Increase font size */
 }
 </style>
